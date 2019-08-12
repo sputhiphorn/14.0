8d3832d51b70c4d473d7e513e81e7ac179d1f107
pchanthou <p.chanthou@khmerdev.com>
2019-08-12 11:14:42 +0700

    new feature for hidden some menu in pos retail

---
+++ kd_pos_retail_hidden/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'POS Retail Hidden Function',
+    'summary': """POS Retail Hidden Function""",
+    'version': '11.0.1.0',
+    'description': """POS Retail Hidden Function""",
+    'author': 'KhmerDev',
+    'company': 'KhmerDev',
+    'website': 'http://www.khmerdev.com',
+    'category': 'Point of Sale',
+    'depends': ['base', 'point_of_sale', 'pos_retail'],
+    'license': 'OPL-1',
+    'data': [
+    	#'views/register.xml',
+    	'views/pos_config.xml'
+    ],
+    # 'qweb': ['static/src/xml/*.xml'],
+    # 'images': ['static/description/banner.png'],
+    'demo': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
