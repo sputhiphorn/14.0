05d19907ab5203954ac347e6c71c60002cf71221
s.sanstar <s.sanstar@khmerdev.com>
2020-03-12 09:45:06 +0700

    takeout update_rate in extend

--- pos_extend/models/pos_config.py
+++ pos_extend/models/pos_config.py
@@ -7,4 +7,3 @@ class pos_config(models.Model):
 
     # Multi Currency
     multi_currency = fields.Boolean('Multi currency', default=0)
-    multi_currency_update_rate = fields.Boolean('Update rate', default=0)
