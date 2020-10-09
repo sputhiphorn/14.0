e6fa727697f22e310c901bb1df1ebed10eb324e4
sanstarsam <48504042+sanstarsam@users.noreply.github.com>
2020-10-09 16:59:52 +0700

    add condition tyle for invoice (#23)
    Co-authored-by: s.sanstar <s.sanstar@khmerdev.com>

--- kd_doc_template_base/models/account_invoice.py
+++ kd_doc_template_base/models/account_invoice.py
@@ -20,32 +20,33 @@ class AccountInvoice(models.Model):
 
     def check_invoice_journal(self):
         try:
-            sequence_tax_withouttax = self.env['ir.config_parameter'].sudo().get_param('kd_invoice-sequence-tax-withouttax.sequence_tax_withouttax')
-            sequence_tax_withouttax_by_warehouse = self.env['ir.config_parameter'].sudo().get_param('kd_invoice-sequence-tax-withouttax.sequence_tax_withouttax_by_warehouse')
-            if sequence_tax_withouttax == 'True':
-                pid = self.env['res.partner']._find_accounting_partner(self.partner_id).id
-                view = self.env['res.partner'].search([('id', '=', pid)], limit=1)
-                tax = super(AccountInvoice, self)._get_tax_amount_by_group()
-                journal_tax_id = int(self.env['ir.config_parameter'].sudo().get_param('kd_invoice-sequence-tax-withouttax.journal_tax_id'))
-                journal_withouttax_id = int(self.env['ir.config_parameter'].sudo().get_param('kd_invoice-sequence-tax-withouttax.journal_withouttax_id'))
-                if not view.vat:
-                    self.journal_id = journal_withouttax_id
-                else:
-                    self.journal_id = journal_tax_id
-            elif sequence_tax_withouttax_by_warehouse == 'True':
-                if len(self.partner_id.warehouse_id._ids) == 0:
-                    raise Warning(_("Cannot find invoice journals for warehouse. You Should Configure it. Please go to Account Configuration."))
-                else:
-                    commercial_invoice = self.env['kd_doc_template_base.commercial_invoice'].search(
-                        [('warehouse_id', '=', self.partner_id.warehouse_id.id)], limit=1)
-                    if len(commercial_invoice) == 0:
+            if self.type == 'out_invoice':
+                sequence_tax_withouttax = self.env['ir.config_parameter'].sudo().get_param('kd_invoice-sequence-tax-withouttax.sequence_tax_withouttax')
+                sequence_tax_withouttax_by_warehouse = self.env['ir.config_parameter'].sudo().get_param('kd_invoice-sequence-tax-withouttax.sequence_tax_withouttax_by_warehouse')
+                if sequence_tax_withouttax == 'True':
+                    pid = self.env['res.partner']._find_accounting_partner(self.partner_id).id
+                    view = self.env['res.partner'].search([('id', '=', pid)], limit=1)
+                    tax = super(AccountInvoice, self)._get_tax_amount_by_group()
+                    journal_tax_id = int(self.env['ir.config_parameter'].sudo().get_param('kd_invoice-sequence-tax-withouttax.journal_tax_id'))
+                    journal_withouttax_id = int(self.env['ir.config_parameter'].sudo().get_param('kd_invoice-sequence-tax-withouttax.journal_withouttax_id'))
+                    if not view.vat:
+                        self.journal_id = journal_withouttax_id
+                    else:
+                        self.journal_id = journal_tax_id
+                elif sequence_tax_withouttax_by_warehouse == 'True':
+                    if len(self.partner_id.warehouse_id._ids) == 0:
                         raise Warning(_("Cannot find invoice journals for warehouse. You Should Configure it. Please go to Account Configuration."))
                     else:
-                        tax = super(AccountInvoice, self)._get_tax_amount_by_group()
-                        if not tax:
-                            self.journal_id = int(commercial_invoice.journal_withouttax_id.id)
+                        commercial_invoice = self.env['kd_doc_template_base.commercial_invoice'].search(
+                            [('warehouse_id', '=', self.partner_id.warehouse_id.id)], limit=1)
+                        if len(commercial_invoice) == 0:
+                            raise Warning(_("Cannot find invoice journals for warehouse. You Should Configure it. Please go to Account Configuration."))
                         else:
-                            self.journal_id = int(commercial_invoice.journal_tax_id.id)
+                            tax = super(AccountInvoice, self)._get_tax_amount_by_group()
+                            if not tax:
+                                self.journal_id = int(commercial_invoice.journal_withouttax_id.id)
+                            else:
+                                self.journal_id = int(commercial_invoice.journal_tax_id.id)
         except:
             raise Warning(_("Cannot find invoice journals for warehouse. You Should Configure it. Please go to Account Configuration."))
         return
